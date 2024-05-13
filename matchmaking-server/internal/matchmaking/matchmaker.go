3d996e8b847f3873637db566c0260c7327ff6dc0
ariel <ariel.pujol@ynov.com>
2024-05-13 12:04:03 +0200

    MatchFinder Upgrade

--- matchmaking-server/internal/matchmaking/matchmaker.go
+++ matchmaking-server/internal/matchmaking/matchmaker.go
@@ -2,9 +2,8 @@ package matchmaking
 
 import (
 	"context"
-	"fmt"
-	"log"
 	"matchmaking-server/pkg/database" // Assurez-vous que le chemin d'importation est correct
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 )
@@ -14,15 +13,37 @@ type Player struct {
 	SkillLevel int
 }
 
-func FindMatch(player Player) {
+func FindMatchForPlayer(player Player) (*Player, error) {
 	collection := database.GetCollection("players")
-	// Utilisation de bson.M pour le filtre
-	filter := bson.M{"skillLevel": bson.M{"$eq": player.SkillLevel}} // Exemple simpliste
-	var opponent Player
-	err := collection.FindOne(context.Background(), filter).Decode(&opponent)
+	filter := bson.M{"skillLevel": bson.M{"$eq": player.SkillLevel}}
+	var matchedPlayer Player
+
+	err := collection.FindOne(context.Background(), filter).Decode(&matchedPlayer)
 	if err != nil {
-		log.Println("No match found:", err)
-	} else {
-		fmt.Println("Match found:", opponent.Username)
+		return nil, err // Aucun joueur correspondant trouvé ou erreur de DB
 	}
+	return &matchedPlayer, nil
+}
+
+func FindMatchWithTimeAdjustment(player Player, startTime time.Time) (*Player, error) {
+	collection := database.GetCollection("players")
+	currentTime := time.Now()
+	elapsed := currentTime.Sub(startTime)
+
+	// Augmenter la tolérance de niveau de compétence toutes les 30 secondes
+	tolerance := int(elapsed.Seconds() / 30)
+
+	filter := bson.M{
+		"skillLevel": bson.M{
+			"$gte": player.SkillLevel - tolerance,
+			"$lte": player.SkillLevel + tolerance,
+		},
+	}
+	var matchedPlayer Player
+
+	err := collection.FindOne(context.Background(), filter).Decode(&matchedPlayer)
+	if err != nil {
+		return nil, err // Aucun joueur correspondant trouvé ou erreur de DB
+	}
+	return &matchedPlayer, nil
 }
